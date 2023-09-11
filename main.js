d7dbf381e10161499b4e33150ddb83dc831bb422
Han7726816 <133084536+Han7726816@users.noreply.github.com>
2023-09-11 13:57:46 +0800

    more commits

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let rightArrow = document.querySelector(".right-arrow")
 let leftArrow = document.querySelector(".left-arrow")
 let container = document.querySelector(".container")
+let darken = document.querySelector(".darken")
 let books = document.querySelectorAll('.book')
+let popups = Array.from(document.querySelectorAll('.popup'))
+let clickables = document.querySelectorAll('.clickable')
 let selectedBook = ''
-let bookOrder = Array.from(books).map(b => {return b.dataset.order})
+let bookOrder = Array.from(books)
+let correctBookOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 rightArrow.addEventListener('click', () => {
     container.classList.add('move')
@@ -17,32 +21,66 @@ leftArrow.addEventListener('click', () => {
     leftArrow.classList.remove('appear')
 })
 
+darken.addEventListener('click', () => {
+    for (const popup of popups) {
+        popup.classList.remove('appear')
+    }
+    darken.classList.remove('appear')
+})
+
 for (const book of books) {
-    book.addEventListener('click', () => {
-        if (selectedBook == '') {
-            selectedBook = book
-            book.classList.add('selected')
-        } else if (selectedBook == book) {
-            selectedBook = ''
-            book.classList.remove('selected')
-        } else bookSwap(selectedBook, book)
+    book.addEventListener('click', selectBook)
+}
+
+for (const clickable of clickables) {
+    clickable.addEventListener('click', () => {
+        let correspondingPopup = popups.filter(p => {return p.classList.contains(clickable.id)})[0]
+
+        correspondingPopup.classList.add('appear')
+        darken.classList.add('appear')
     })
 }
 
 function bookSwap(book1, book2) {
     let order1 = book1.style.order
     let order2 = book2.style.order
-    let dataOrder1 = book1.dataset.order
-    let dataOrder2 = book2.dataset.order
+    let numberOrder
+    let isCorrect = true
     
     book2.style.order = order1
     book1.style.order = order2
     book1.classList.remove('selected')
     book2.classList.remove('selected')
+    
+    bookOrder.sort((a, b) => {return a.style.order.toString() - b.style.order.toString()})
+
+    numberOrder = bookOrder.map(b => {return b.dataset.order.toString()})
     selectedBook = ''
 
-    console.log(bookOrder); 
-    bookOrder[bookOrder.indexOf(dataOrder1)] = dataOrder2
-    bookOrder[bookOrder.indexOf(dataOrder2)] = dataOrder1
-    console.log(bookOrder); 
+    for (let i = 0; i < numberOrder.length; i++) {
+        const num1 = numberOrder[i];
+        const num2 = correctBookOrder[i];
+
+        if (num1 != num2) {
+            isCorrect = false
+            break
+        }
+    }
+
+    if (isCorrect) {
+        for (const book of bookOrder) {
+            book.removeEventListener('click', selectBook)
+        }
+    }
+}
+
+function selectBook(e) {
+    let book = e.target
+    if (selectedBook == '') {
+        selectedBook = book
+        book.classList.add('selected')
+    } else if (selectedBook == book) {
+        selectedBook = ''
+        book.classList.remove('selected')
+    } else bookSwap(selectedBook, book)
 }
